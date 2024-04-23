0fb4f99b9eaa9c8e8299fe9f8c4e42670e2e7358
Matheus Heck <matheus@conduite.tv>
2024-04-23 23:19:22 +0200

    Chore: refactor assigns

--- lib/multiplayer_game_web/plugs/assign_session.ex
+++ lib/multiplayer_game_web/plugs/assign_session.ex
@@ -1,22 +1,26 @@
 defmodule MultiplayerGameWeb.Plug.AssignSession do
   def init(opts), do: opts
 
-  def call(conn = %{session: %{id: _}}, _opts) do
-    conn
-  end
-
   def call(conn, _opts) do
     with {:ok, _} <- MultiplayerGame.Game.State.maybe_start_game() do
-      %{id: id, name: name} = player = MultiplayerGame.Player.create()
-      MultiplayerGame.Game.maybe_add_player(player)
+      case Plug.Conn.get_session(conn, :id) do
+        nil ->
+          put_session(conn)
 
-      conn =
-        conn
-        |> Plug.Conn.put_session(:unique_name, name)
-        |> Plug.Conn.put_session(:id, id)
-        |> Plug.Conn.put_session(:player, player)
-
-      conn
+        player_id ->
+          conn
+      end
     end
+  end
+
+  defp put_session(conn) do
+    %{id: id, name: name} = player = MultiplayerGame.Player.create()
+
+    conn =
+      conn
+      |> Plug.Conn.put_session(:unique_name, name)
+      |> Plug.Conn.put_session(:id, id)
+
+    conn
   end
 end
