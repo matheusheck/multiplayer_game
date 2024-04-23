820d94e30c6f7c916cafcba3fa856bb351a204bf
Matheus Heck <matheus@conduite.tv>
2024-04-23 23:17:25 +0200

    Chore: clean JS and prepare copy hook

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,43 +2,29 @@
 import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
-// import { listener } from "./input"
-import { renderScreen } from "./render"
 
-let hooks = {};
-let current_player_id = null
-
-hooks.Listener = {
-    mounted() {
-        window.addEventListener('keydown', e => this.handleKeydown(e))
-    },
-
-    handleKeydown(event) {
-        console.log(event)
-        const keyPressed = event.key
-    
-        const command = {
-            type: 'move-player',
-            playerId: current_player_id,
-            keyPressed
-        }
-    
-        this.pushEvent("key_down", command)
+let hooks = {
+    CopyToClipboard: {
+       mounted() {
+         this.handleEvent("copy_to_clipboard", ({text}) => {
+            console.log("chegou")
+           window.copyToClipboard(text);
+         });
+       }
     }
-  };
-  
-
-window.addEventListener(
-    "phx:game",
-    game => {
-        console.log("Game Received")
-        current_player_id = game.detail.current_player
-        renderScreen(game.detail, requestAnimationFrame, game.detail.current_player)
-    }
-)
+   };
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: hooks})
 liveSocket.connect()
 
 
+window.copyToClipboard = function(text) {
+    navigator.clipboard.writeText(text).then(function() {
+       console.log('Copying to clipboard was successful!');
+    }, function(err) {
+       console.error('Could not copy text: ', err);
+    });
+   };
+
+   
